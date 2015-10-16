85ddda773a84381f4d9ae5e8a896a54dd281c289
eyoung100 <ecyoung@mygrande.net>
2015-10-15 20:58:18 -0500

    Added Story Class to Controller.

--- app/src/main/java/carterswebs/com/interactivestory/model/Choice.java
+++ app/src/main/java/carterswebs/com/interactivestory/model/Choice.java
@@ -4,4 +4,19 @@ public class Choice {
     private String mText;
     private int mNextPage;
 
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
 }
--- app/src/main/java/carterswebs/com/interactivestory/model/Page.java
+++ app/src/main/java/carterswebs/com/interactivestory/model/Page.java
@@ -13,4 +13,28 @@ public class Page {
     public void setImageId (int id) {
         mImageId = id;
     }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public Choice getChoice1() {
+        return mChoice1;
+    }
+
+    public void setChoice1(Choice choice1) {
+        mChoice1 = choice1;
+    }
+
+    public Choice getChoice2() {
+        return mChoice2;
+    }
+
+    public void setChoice2(Choice choice2) {
+        mChoice2 = choice2;
+    }
 }
